71aec5158ba680d17ed82b08742cbacae44079b5
SARA SIMON <smbsimon@gmail.com>
2016-12-27 15:37:38 -0500

    show route to the local version, not route to the prod version

--- audio-player/query.py
+++ audio-player/query.py
@@ -16,7 +16,7 @@ from oauth2client.client import SignedJwtAssertionCredentials
 
 
 def get_google_sheet():
-    json_key = json.load(open('/home/vprnet/webapps/crons/crons/homepage.json'))
+    json_key = json.load(open('homepage.json'))
     scope = ['https://spreadsheets.google.com/feeds']
     credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
     authorization = gspread.authorize(credentials)
